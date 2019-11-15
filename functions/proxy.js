f5d924d9f9627231a94f03e531c1ad0c5a59034d
Jesmar Castillo <jesmar@ucsb.edu>
2019-11-14 17:01:22 -0800

    Add admin endpoint

--- functions/proxy.js
+++ functions/proxy.js
@@ -7,21 +7,56 @@ const app = express();
 
 app.use(cors({ origin: true }));
 
+const getClusterInfoWare = async (req, res, next) => {
+  try {
+    req.cluster = await firebaseWare.getClusterInfo(req.params.cid);
+    next();
+  } catch (error) {
+    res.status(404);
+    next(error);
+  }
+};
+
 app.use(
   '/clusters/:cid',
+  getClusterInfoWare,
+  proxy({
+    target: 'https://localhost',
+    router: req => `https://${req.cluster.addr}`,
+    pathRewrite: { '^/clusters/[^/]+': '' },
+    changeOrigin: true,
+    logLevel: 'silent'
+  }),
+  (err, req, res, next) => {
+    res.json({ status: 'Failure', message: err.message, code: res.statusCode });
+  }
+);
+
+app.use(
+  '/admin/clusters/:cid',
+  firebaseWare.verifyToken,
+  getClusterInfoWare,
   async (req, res, next) => {
     try {
-      req.cluster = await firebaseWare.getClusterInfo(req.params.cid);
-      next();
+      const {
+        decodedToken: { uid }
+      } = res.locals;
+      if (uid === req.cluster.owner) {
+        const adminToken = await firebaseWare.createCustomToken();
+        req.headers.authorization = `Bearer ${adminToken}`;
+        next();
+      } else {
+        throw new Error("You're not the owner of this cluster.");
+      }
     } catch (error) {
-      res.status(404);
+      res.status(403);
       next(error);
     }
   },
   proxy({
     target: 'https://localhost',
     router: req => `https://${req.cluster.addr}`,
-    pathRewrite: { '^/clusters/[^/]+': '' },
+    pathRewrite: { '^/admin/clusters/[^/]+': '' },
     changeOrigin: true,
     logLevel: 'silent'
   }),
